00e3c9794b8a09bb48560be50fd63eeace7f3b5a
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2022-03-06 14:27:51 +0100

    mini refactoring

--- app/src/main/java/com/hakansoft/polishdictionary/HomeSearchFragment.kt
+++ app/src/main/java/com/hakansoft/polishdictionary/HomeSearchFragment.kt
@@ -11,34 +11,18 @@ import android.widget.EditText
 import android.widget.Spinner
 import androidx.navigation.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [HomeSearchFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class HomeSearchFragment : Fragment() {
-    //private lateinit var binding: FragmentHomeSearchBinding
-    //private lateinit var searchViewModel: HomeSearchViewModel
 
     @SuppressLint("UseRequireInsteadOfGet")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        /*
-        searchViewModel = ViewModelProvider(this).get(HomeSearchViewModel::class.java)
-        binding = FragmentHomeSearchBinding.inflate(layoutInflater)
-        binding.lifecycleOwner = this
-        binding.viewModel = searchViewModel
-
-        var view = binding.root
-        return view
-        */
 
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_home_search, container, false)
@@ -57,16 +41,4 @@ class HomeSearchFragment : Fragment() {
 
         return view
     }
-
-    /*
-    fun search(view: View) {
-        val partOfSpeechSpinner: Spinner = requireView().findViewById(R.id.spinner_partofspeech)
-        val partOfSpeech = partOfSpeechSpinner.selectedItem.toString()
-        val searchTermEditText : EditText = requireView().findViewById(R.id.search_box)
-        val searchTerm = searchTermEditText.text.toString()
-        val action = HomeSearchFragmentDirections.searchAction(searchTerm, partOfSpeech)
-        view.findNavController().navigate(action)
-    }
-
-     */
 }
--- app/src/main/java/com/hakansoft/polishdictionary/MainActivity.kt
+++ app/src/main/java/com/hakansoft/polishdictionary/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.hakansoft.polishdictionary
 
 import android.os.Bundle
-import android.view.View
-import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 import android.view.Window
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
