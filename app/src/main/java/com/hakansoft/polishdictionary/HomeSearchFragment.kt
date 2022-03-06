a8f6ff7871052d3d3bfc36e9d323ccc2472f9027
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2022-03-06 17:04:00 +0100

    Navigation to search page, searchbox single line input, navbar improvements #7 #8

--- app/src/main/java/com/hakansoft/polishdictionary/HomeSearchFragment.kt
+++ app/src/main/java/com/hakansoft/polishdictionary/HomeSearchFragment.kt
@@ -1,15 +1,20 @@
 package com.hakansoft.polishdictionary
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView.OnEditorActionListener
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
 
 /**
  * A simple [Fragment] subclass.
@@ -18,11 +23,15 @@ import androidx.navigation.findNavController
  */
 class HomeSearchFragment : Fragment() {
 
+    val args: HomeSearchFragmentArgs by navArgs()
+
     @SuppressLint("UseRequireInsteadOfGet")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        var focusOnSearchbox: Boolean = args.focusOnSearchbox
 
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_home_search, container, false)
@@ -35,8 +44,26 @@ class HomeSearchFragment : Fragment() {
                 val searchTermEditText : EditText = view.findViewById(R.id.search_box)
                 val searchTerm = searchTermEditText.text.toString()
                 val action = HomeSearchFragmentDirections.searchAction(searchTerm, partOfSpeech)
+                Utility.prepareWindow(getActivity()!!.getWindow(), context!!)
                 view.findNavController().navigate(action)
             }
+        })
+
+        var searchBox = view.findViewById<EditText>(R.id.search_box)
+        if (focusOnSearchbox) {
+            searchBox.requestFocus()
+            val imm: InputMethodManager? = activity!!.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager?
+            imm!!.toggleSoftInput(
+                InputMethodManager.HIDE_IMPLICIT_ONLY,0
+            )
+        }
+
+        searchBox.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                searchButton.callOnClick()
+                return@OnEditorActionListener true
+            }
+            false
         })
 
         return view
--- app/src/main/java/com/hakansoft/polishdictionary/MainActivity.kt
+++ app/src/main/java/com/hakansoft/polishdictionary/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.hakansoft.polishdictionary
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,13 @@ class MainActivity : AppCompatActivity() {
         val window: Window = getWindow()
         supportActionBar?.hide()
         Utility.prepareWindow(window, this)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Utility.prepareWindow(window, this)
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
